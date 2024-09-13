d81a9a40aa98cb9ac24c66265b4b41101a0e6def
linusbayere <linusbayere@gmail.com>
2024-09-13 15:58:41 -0400

    Chapter 3: Sending JSON responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type envelop map[string]any
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -17,4 +20,24 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelop, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	// maps.Insert(w.Header(), maps.All(headers)) - we could also do like this. (added in go 1.23.0)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
